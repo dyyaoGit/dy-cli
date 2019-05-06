29ce7deef0d37548b36b337fa660d529bd2ec301
dyyao <646617513@qq.com>
2019-05-06 23:18:46 +0800

    后台管理页面配置完成

---
+++ README.md
@@ -0,0 +1,3 @@
+# this is a cli for front-end
+
+# 这是一个给前端使用的cli脚手架。
--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-const program = require('commander');
-const inqurier = require('inquirer');
+#!/usr/bin/env node
 
-console.log("这是dyyao的脚手架");
+const program = require("commander");
+const getFrameType = require("./lib/getFrameType");
+
 program
-    .version("0.0.1", '-v, --version')
-    .description("这是一个简单的脚手架测试")
+    .version("1.1.3", '-v, --version')
+    .description("初始化用，省去每个脚手架去除默认文件的繁琐")
     .parse(process.argv);
 
 program
     .command("init <template> <dir>")
     .action(function (template, dir) {
-        console.log("你选择的模板是: " + template);
-        console.log("你输入文件夹名字是： " + dir);
-        inqurier.prompt({
-            message: "是否需要vue的路由？",
-            name: 'isRouter'
-        }).then(res => {
-            console.log(res);
-            console.log(res.isRouter);
-        })
+        getFrameType(dir);
     });
 program.parse(process.argv);
-
-
-
-// if(program.people){
-//     console.log("你点击了p")
-// } else if(program.black){
-//     console.log("你点击了b")
-// }
-// program
-//     .command("* <one> <two>")
-//     .action(function (one, two) {
-//     console.log(one, 'one');
-//     console.log(two, 'two');
-// }).parse(process.argv);
-
-
