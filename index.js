270ce009d94c59c3bdad22f9ff1100bed5502982
dyyao <646617513@qq.com>
2019-08-02 13:46:43 +0800

    添加初始化vue文件模板功能

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const program = require("commander");
 const getFrameType = require("./lib/getFrameType");
+const fsPromise = require('./lib/fsPromise');
+const path = require('path');
 
 program
     .usage("dy init webpack [project-name]")
@@ -13,5 +15,15 @@ program
     .command("init <template> <dir>")
     .action(function (template, dir) {
         getFrameType(dir);
+    });
+
+program
+    .command("new <template> <file-name>")
+    .action(async function (template, filename) {
+        let data = await fsPromise.readFile(path.join(__dirname, 'lib/template.vue'));
+        let reg = new RegExp(/test/, 'g');
+        let newData = data.replace(reg, filename);
+        await fsPromise.writeFile(process.cwd() + '/' + filename + '.vue', newData);
+        console.log('文件新建完成');
     });
 program.parse(process.argv);
